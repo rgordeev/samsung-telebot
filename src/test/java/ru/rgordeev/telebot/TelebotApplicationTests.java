8150485483160bb8faaaddc28ee7bccb4fd5e02e
rgordeev <roma.gordeev@gmail.com>
2019-11-15 19:36:06 +0300

    initial commit

---
+++ src/test/java/ru/rgordeev/telebot/TelebotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rgordeev.telebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelebotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
